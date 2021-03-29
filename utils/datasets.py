84e2a6a1fcd90bfd30592cee37bd39876dca72e6
Abhishek Pandey <43136439+abhishek776655@users.noreply.github.com>
2021-03-29 22:18:08 +0530

    Update datasets.py

--- utils/datasets.py
+++ utils/datasets.py
@@ -375,7 +375,7 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
         # Check cache
         self.label_files = img2label_paths(self.img_files)  # labels
         # cache_path = (p if p.is_file() else Path(self.label_files[0]).parent).with_suffix('.cache')  # cached labels
-        cache_path = Path("./yolov5/data/")
+        cache_path = Path("/kaggle/working/yolov5/data/")
         if cache_path.is_file():
             cache, exists = torch.load(cache_path), True  # load
             if cache['hash'] != get_hash(self.label_files + self.img_files) or 'version' not in cache:  # changed
